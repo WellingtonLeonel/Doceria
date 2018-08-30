c225d72cecc6c66ef35754520e84587b68aa66af
wellington leonel <wellington.ius.direito@hotmail.com>
2018-08-30 19:36:43 -0300

    Inicio segunda tela

---
+++ DoceriaTela/DoceriaTela/views/Tabela2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoceriaTela.views
+{
+    /// <summary>
+    /// Lógica interna para Tabela2.xaml
+    /// </summary>
+    public partial class Tabela2 : Window
+    {
+        public Tabela2()
+        {
+            InitializeComponent();
+        }
+    }
+}
