54102982cfceb201c823ed60858adf1f1786993a
wellington leonel <wellington.ius.direito@hotmail.com>
2018-08-30 20:13:09 -0300

    Finalizado segunda tela

--- DoceriaTela/DoceriaTela/views/Tabela2.xaml.cs
+++ DoceriaTela/DoceriaTela/views/Tabela2.xaml.cs
@@ -23,5 +23,10 @@ namespace DoceriaTela.views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
